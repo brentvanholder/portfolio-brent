5efbcfe437a44d23b3b4f874ee1005b8728b6204
Brent Van Holder <brent@askphill.com>
2022-10-06 18:00:08 +0200

    refactor(navbar.js)

--- app/javascript/plugins/navbar.js
+++ app/javascript/plugins/navbar.js
@@ -1,14 +1,11 @@
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { scrollNavbar() };
+const navbar = document.getElementById("nav");
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 const scrollNavbar = () => {
-  var navbar = document.getElementById("nav");
-
-  if (window.pageYOffset > 0) {
-    navbar.classList.toggle("transparant")
-  }
+  navbar.classList[window.scrollY > 0 ? "remove" : "add"]("transparant")
 }
+
+// When the user scrolls the page, execute myFunction
+window.addEventListener('scroll', scrollNavbar);
 
 export { scrollNavbar };
