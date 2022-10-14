b1e92d80ad5f6bc8b41a167d84726d9854f51157
Brent Van Holder <brent@askphill.com>
2022-10-14 17:37:41 +0200

    feat(tailwind animation)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,29 @@
 module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sora': ['Sora', 'sans-serif']
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { transform: 'translateY(-50px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '100' }
+        },
+        fadeOut: {
+          '0%': { transform: 'translateY(20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '100' }
+        },
+      },
+      animation: {
+        'fadein2': 'fadeIn 2s',
+        'fadein3': 'fadeIn 2.1s',
+        'fadein5': 'fadeIn 2.2s',
+        'fadeOut2': 'fadeOut 1.2s',
+        'fadeOut3': 'fadeOut 1.3s',
+        'fadeOut5': 'fadeOut 1.4s'
+      },
+    },
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
