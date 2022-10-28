33e8ce789ded5b16db91e1a74722ad1edf8efe26
Brent Van Holder <brent@askphill.com>
2022-10-28 17:28:27 +0100

    refactor(hello_controller.js)

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = ["nav", "image"]
+
+  layout() {
+    this.imageTarget.style.top = `-${window.scrollY}px`;
+    window.scrollY > 0 ? this.navTarget.classList.add('opacity-0', 'duration-500', 'transition-all') : this.navTarget.classList.remove('opacity-0', 'duration-500', 'transition-all');
   }
 }
