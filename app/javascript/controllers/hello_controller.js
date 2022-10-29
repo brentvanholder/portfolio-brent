3ad4022de75b7717c0efe5e0d60c8bbcd3508b5a
Brent Van Holder <brent@askphill.com>
2022-10-29 14:08:10 +0100

    style(hello_controller.js): about

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["nav", "image"]
+  static targets = ["nav", "image", 'about']
+
 
   layout() {
+    const textContainerOffsetTop = window.pageYOffset + this.aboutTarget.getBoundingClientRect().top
+    if (window.scrollY > textContainerOffsetTop) this.aboutTarget.classList.add('animate-translateAbout')
     this.imageTarget.style.top = `-${window.scrollY}px`;
     window.scrollY > 0 ? this.navTarget.classList.add('opacity-0', 'duration-500', 'transition-all') : this.navTarget.classList.remove('opacity-0', 'duration-500', 'transition-all');
   }
